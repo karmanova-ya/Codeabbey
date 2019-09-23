a3a6d8b18309e08dc4e9e3c6e85114b9c9585ded
yana <karmanova.yana@gmaail.com>
2019-09-23 14:14:44 +0200

    Initial commit

---
+++ src/Summ.java
@@ -0,0 +1,17 @@
+//Sum "A+B" 22.03.2018
+
+public class Summ {
+    private int a;
+    private int b;
+    private int c;
+
+    Summ (int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public void countSumm(){
+        c=a+b;
+        System.out.println(c);
+    }
+}
